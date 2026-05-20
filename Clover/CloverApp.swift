359ebbbffb028c77c984776b18705a4c09ba36ef
Elisabeth Everhart <eeverhart@wm.edu>
2026-05-20 16:19:28 -0400

    Initial Commit

---
+++ Clover/CloverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloverApp.swift
+//  Clover
+//
+//  Created by Elisabeth Everhart on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CloverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloverTests/CloverTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloverTests.swift
+//  CloverTests
+//
+//  Created by Elisabeth Everhart on 5/20/26.
+//
+
+import Testing
+@testable import Clover
+
+struct CloverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
